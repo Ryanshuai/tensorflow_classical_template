1cd9a8f601cd08a4eda00594da90b902a3b8c1bf
Ryan <1018718155@qq.com>
2017-12-04 08:37:04 +0800

    1

--- VAE/VAE.py
+++ VAE/VAE.py
@@ -26,7 +26,7 @@ class VAE(object):
         tensor_inputs = tf.convert_to_tensor(X)  # [BS,W,H,D]=[BS,128,128,3]
 
         # conv1  #[BS,128,128,3]->[BS,64,64,64]
-        W_conv1 = tf.get_variable('W_conv1', [5, 5, 3, 64],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_conv1 = tf.get_variable('W_conv1', [5, 5, 3, 64], initializer=tf.contrib.layers.xavier_initializer_conv2d())
         b_conv1 = tf.get_variable('b_conv1', initializer=tf.constant(0.))
         z_conv1 = tf.nn.conv2d(tensor_inputs / 255, W_conv1, strides=[1, 2, 2, 1], padding='SAME') + b_conv1
         mean_conv1, variance_conv1 = tf.nn.moments(z_conv1, axes=[0, 1, 2])
@@ -36,7 +36,7 @@ class VAE(object):
         a_conv1 = tf.nn.leaky_relu(bn_conv1)
 
         # conv2  #[BS,64,64,64]->[BS,32,32,128]
-        W_conv2 = tf.get_variable('W_conv2', [5, 5, 64, 128],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_conv2 = tf.get_variable('W_conv2', [5, 5, 64, 128],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         b_conv2 = tf.get_variable('b_conv2', initializer=tf.constant(0.))
         z_conv2 = tf.nn.conv2d(a_conv1, W_conv2, strides=[1, 2, 2, 1], padding='SAME') + b_conv2
         mean_conv2, variance_conv2 = tf.nn.moments(z_conv2, axes=[0, 1, 2])
@@ -46,7 +46,7 @@ class VAE(object):
         a_conv2 = tf.nn.leaky_relu(bn_conv2)
 
         # conv3  #[BS,32,32,128]->[BS,16,16,256]
-        W_conv3 = tf.get_variable('W_conv3', [5, 5, 128, 256],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_conv3 = tf.get_variable('W_conv3', [5, 5, 128, 256],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         b_conv3 = tf.get_variable('b_conv3', initializer=tf.constant(0.))
         z_conv3 = tf.nn.conv2d(a_conv2, W_conv3, strides=[1, 2, 2, 1], padding='SAME') + b_conv3
         mean_conv3, variance_conv3 = tf.nn.moments(z_conv3, axes=[0, 1, 2])
@@ -56,7 +56,7 @@ class VAE(object):
         a_conv3 = tf.nn.leaky_relu(bn_conv3)
 
         # conv4  #[BS,16,16,256]->[BS,8,8,512]
-        W_conv4 = tf.get_variable('W_conv4', [5, 5, 256, 512],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_conv4 = tf.get_variable('W_conv4', [5, 5, 256, 512],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         b_conv4 = tf.get_variable('b_conv4', initializer=tf.constant(0.))
         z_conv4 = tf.nn.conv2d(a_conv3, W_conv4, strides=[1, 2, 2, 1], padding='SAME') + b_conv4
         mean_conv4, variance_conv4 = tf.nn.moments(z_conv4, axes=[0, 1, 2])
@@ -69,7 +69,7 @@ class VAE(object):
         flatten = tf.reshape(a_conv4, [self.BS, -1])
 
         # fc1 #[BS,32768]->[BS,1024]
-        W_fc1 = tf.get_variable('W_fc1', [flatten.shape[1].value, 1024],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_fc1 = tf.get_variable('W_fc1', [flatten.shape[1].value, 1024],initializer=tf.contrib.layers.xavier_initializer())
         b_fc1 = tf.get_variable('b_fc1', [1024], initializer=tf.constant_initializer(0.))
         z_fc1 = tf.matmul(flatten, W_fc1) + b_fc1
         mean_fc1, variance_fc1 = tf.nn.moments(z_fc1, axes=[0])
@@ -79,7 +79,7 @@ class VAE(object):
         a_fc1 = tf.nn.leaky_relu(bn_fc1)
 
         # fc1 #[BS,1024]->[BS,2*z_dim]
-        W_fc2 = tf.get_variable('W_fc2', [1024, 2 * self.z_dim],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_fc2 = tf.get_variable('W_fc2', [1024, 2 * self.z_dim],initializer=tf.contrib.layers.xavier_initializer())
         b_fc2 = tf.get_variable('b_fc2', [2 * self.z_dim], initializer=tf.constant_initializer(0.))
         z_fc2 = tf.matmul(a_fc1, W_fc2) + b_fc2
         mean_fc2, variance_fc2 = tf.nn.moments(z_fc2, axes=[0])
@@ -96,7 +96,7 @@ class VAE(object):
     def _decoder(self, z):
         tensor_z = tf.convert_to_tensor(z)  # [BS,vec_size]
         # defc
-        W_defc = tf.get_variable('W_defc', [tensor_z.shape[1].value, 4 * 4 * 1024],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_defc = tf.get_variable('W_defc', [tensor_z.shape[1].value, 4 * 4 * 1024],initializer=tf.contrib.layers.xavier_initializer())
         b_defc = tf.get_variable('b_defc', [4 * 4 * 1024], initializer=tf.constant_initializer(0.))
         z_defc1 = tf.matmul(tensor_z, W_defc) + b_defc
         # deflatten  # [BS,4*4*512]->[BS,4,4,512]
@@ -109,7 +109,7 @@ class VAE(object):
         a_deconv0 = tf.nn.relu(bn_deconv0)
 
         # deconv1  # [BS,4,4,1024]->[BS,8,8,512]
-        W_deconv1 = tf.get_variable('W_deconv1', [5, 5, 512, 1024],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_deconv1 = tf.get_variable('W_deconv1', [5, 5, 512, 1024],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         z_deconv1 = tf.nn.conv2d_transpose(a_deconv0, W_deconv1, [self.BS, 8, 8, 512], [1, 2, 2, 1])
         mean_deconv1, variance_deconv1 = tf.nn.moments(z_deconv1, axes=[0, 1, 2])
         offset_deconv1 = tf.get_variable('offset_deconv1', initializer=tf.zeros([512]))
@@ -118,7 +118,7 @@ class VAE(object):
         a_deconv1 = tf.nn.relu(bn_deconv1)
 
         # deconv2  # [BS,8,8,512]->[BS,16,16,256]
-        W_deconv2 = tf.get_variable('W_deconv2', [5, 5, 256, 512],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_deconv2 = tf.get_variable('W_deconv2', [5, 5, 256, 512],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         z_deconv2 = tf.nn.conv2d_transpose(a_deconv1, W_deconv2, [self.BS, 16, 16, 256], [1, 2, 2, 1])
         mean_deconv2, variance_deconv2 = tf.nn.moments(z_deconv2, axes=[0, 1, 2])
         offset_deconv2 = tf.get_variable('offset_deconv2', initializer=tf.zeros([256]))
@@ -127,7 +127,7 @@ class VAE(object):
         a_deconv2 = tf.nn.relu(bn_deconv2)
 
         # deconv3  # [BS,16,16,256]->[BS,32,32,128]
-        W_deconv3 = tf.get_variable('W_deconv3', [5, 5, 128, 256],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_deconv3 = tf.get_variable('W_deconv3', [5, 5, 128, 256],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         z_deconv3 = tf.nn.conv2d_transpose(a_deconv2, W_deconv3, [self.BS, 32, 32, 128], [1, 2, 2, 1])
         mean_deconv3, variance_deconv3 = tf.nn.moments(z_deconv3, axes=[0, 1, 2])
         offset_deconv3 = tf.get_variable('offset_deconv3', initializer=tf.zeros([128]))
@@ -136,7 +136,7 @@ class VAE(object):
         a_deconv3 = tf.nn.relu(bn_deconv3)
 
         # deconv4  # [BS,32,32,128]->[BS,64,64,64]
-        W_deconv4 = tf.get_variable('W_deconv4', [5, 5, 64, 128],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_deconv4 = tf.get_variable('W_deconv4', [5, 5, 64, 128],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         z_deconv4 = tf.nn.conv2d_transpose(a_deconv3, W_deconv4, [self.BS, 64, 64, 64], [1, 2, 2, 1])
         mean_deconv4, variance_deconv4 = tf.nn.moments(z_deconv4, axes=[0, 1, 2])
         offset_deconv4 = tf.get_variable('offset_deconv4', initializer=tf.zeros([64]))
@@ -145,7 +145,7 @@ class VAE(object):
         a_deconv4 = tf.nn.relu(bn_deconv4)
 
         # deconv5  # [BS,64,64,64]->[BS,128,128,3]
-        W_deconv5 = tf.get_variable('W_deconv5', [5, 5, 3, 64],initializer=tf.truncated_normal_initializer(stddev=0.02))
+        W_deconv5 = tf.get_variable('W_deconv5', [5, 5, 3, 64],initializer=tf.contrib.layers.xavier_initializer_conv2d())
         z_deconv5 = tf.nn.conv2d_transpose(a_deconv4, W_deconv5, [self.BS, 128, 128, 3], [1, 2, 2, 1])
 
         return z_deconv5 #recon_X
@@ -161,8 +161,8 @@ class VAE(object):
         self.z = self.mu + self.sigma * tf.random_normal(tf.shape(self.mu), 0, 1, dtype=tf.float32) #[bs, z_dim]
         self.recon_X = self._decoder(self.z)
         # loss
-        IO_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.recon_X, labels=self._X),[1, 2, 3])
-        KL_loss = 0.5 * tf.reduce_sum(tf.square(self.mu) + tf.square(self.sigma) - tf.log(1e-8 + tf.square(self.sigma)) - 1,[1])
+        IO_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.recon_X, labels=self._X),[1, 2, 3])# [bs,w,h,c]->[bs,1]
+        KL_loss = 0.5 * tf.reduce_mean(tf.square(self.mu) + tf.square(self.sigma) - tf.log(1e-8 + tf.square(self.sigma)) - 1,[1])# [bs,z_dim]->[bs,1]
         self.IO_loss = tf.reduce_mean(IO_loss)
         self.KL_loss = tf.reduce_mean(KL_loss)
         self.loss = self.IO_loss + self.KL_loss
@@ -202,7 +202,6 @@ class VAE(object):
                     X, label = mnist.train.next_batch(self.BS)
                     X = np.reshape(X, [self.BS, 28, 28, 1])
                     X = np.concatenate((X, X, X), axis=3)
-                    X = X/255
                     #train
                     _, sum_merge, loss, IO_loss, KL_loss = sess.run(
                         [self.optimizer, self.sum_merge, self.loss, self.IO_loss, self.KL_loss],
